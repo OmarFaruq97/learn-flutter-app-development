8a6f66eb98215430a61ae4a40a065b10cd95e6e0
Omar Faruq <omarfaruq9723@gmail.com>
2025-06-16 13:08:51 +0600

    init

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,122 +1,342 @@
+/// A simplified brick-breaker game,
+/// built using the Flame game engine for Flutter.
+///
+/// To learn how to build a more complete version of this game yourself,
+/// check out the codelab at https://flutter.dev/to/brick-breaker.
+library;
+
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GameApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GameApp extends StatefulWidget {
+  const GameApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<GameApp> createState() => _GameAppState();
+}
+
+class _GameAppState extends State<GameApp> {
+  late final BrickBreaker game;
+
+  @override
+  void initState() {
+    super.initState();
+    game = BrickBreaker();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xffa9d6e5), Color(0xfff2e8cf)],
             ),
-          ],
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Center(
+                child: FittedBox(
+                  child: SizedBox(
+                    width: gameWidth,
+                    height: gameHeight,
+                    child: GameWidget(game: game),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class BrickBreaker extends FlameGame
+    with HasCollisionDetection, KeyboardEvents, TapDetector {
+  BrickBreaker()
+      : super(
+    camera: CameraComponent.withFixedResolution(
+      width: gameWidth,
+      height: gameHeight,
+    ),
+  );
+
+  final rand = math.Random();
+
+  double get width => size.x;
+
+  double get height => size.y;
+
+  @override
+  FutureOr<void> onLoad() async {
+    super.onLoad();
+    camera.viewfinder.anchor = Anchor.topLeft;
+    world.add(PlayArea());
+    startGame();
+  }
+
+  void startGame() {
+    world.removeAll(world.children.query<Ball>());
+    world.removeAll(world.children.query<Paddle>());
+    world.removeAll(world.children.query<Brick>());
+
+    world.add(
+      Ball(
+        difficultyModifier: difficultyModifier,
+        radius: ballRadius,
+        position: size / 2,
+        velocity: Vector2(
+          (rand.nextDouble() - 0.5) * width,
+          height * 0.3,
+        ).normalized()
+          ..scale(height / 4),
+      ),
+    );
+
+    world.add(
+      Paddle(
+        size: Vector2(paddleWidth, paddleHeight),
+        cornerRadius: const Radius.circular(ballRadius / 2),
+        position: Vector2(width / 2, height * 0.95),
+      ),
+    );
+
+    world.addAll([
+    for (var i = 0; i < brickColors.length; i++)
+    for (var j = 1; j <= 6; j++)
+      Brick(
+        Vector2(
+            (i + 0.5) * brickWidth + (i + 1) * brickGutter,
+            (j + 2.0) * brickHeight + j * brickGutter,
+            j < = 3
+        ?brickColors[1]
+            :brickColors[brickColors.length-(i+1)],
+      ),
+    brickColors[i],
+
+
+    ),
+    ]);
+  }
+
+  @override
+  void onTap() {
+    super.onTap();
+    startGame();
+  }
+
+  @override
+  KeyEventResult onKeyEvent(KeyEvent event,
+      Set<LogicalKeyboardKey> keysPressed,) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.space:
+      case LogicalKeyboardKey.enter:
+        startGame();
+    }
+    return KeyEventResult.handled;
+  }
+
+  @override
+  Color backgroundColor() => const Color(0xfff2e8cf);
+}
+
+class Ball extends CircleComponent
+    with CollisionCallbacks, HasGameReference<BrickBreaker> {
+  Ball({
+    required this.velocity,
+    required super.position,
+    required double radius,
+    required this.difficultyModifier,
+  }) : super(
+    radius: radius,
+    anchor: Anchor.center,
+    paint: Paint()
+      ..color = const Color(0xff921e1e)
+      ..style = PaintingStyle.fill,
+    children: [CircleHitbox()],
+  );
+
+  final Vector2 velocity;
+  final double difficultyModifier;
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position += velocity * dt;
+  }
+
+  @override
+  void onCollisionStart(Set<Vector2> intersectionPoints,
+      PositionComponent other,) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is PlayArea) {
+      if (intersectionPoints.first.y <= 0) {
+        velocity.y = -velocity.y;
+      } else if (intersectionPoints.first.x <= 0) {
+        velocity.x = -velocity.x;
+      } else if (intersectionPoints.first.x >= game.width) {
+        velocity.x = -velocity.x;
+      } else if (intersectionPoints.first.y >= game.height) {
+        add(
+          RemoveEffect(
+            delay: 0.35,
+            onComplete: () {
+              game.startGame();
+            },
+          ),
+        );
+      }
+    } else if (other is Paddle) {
+      velocity.y = -velocity.y;
+      velocity.x =
+          velocity.x +
+              (position.x - other.position.x) / other.size.x * game.width * 0.3;
+    } else if (other is Brick) {
+      if (position.y < other.position.y - other.size.y / 2) {
+        velocity.y = -velocity.y;
+      } else if (position.y > other.position.y + other.size.y / 2) {
+        velocity.y = -velocity.y;
+      } else if (position.x < other.position.x) {
+        velocity.x = -velocity.x;
+      } else if (position.x > other.position.x) {
+        velocity.x = -velocity.x;
+      }
+      velocity.setFrom(velocity * difficultyModifier);
+    }
+  }
+}
+
+class Paddle extends PositionComponent
+    with DragCallbacks, HasGameReference<BrickBreaker>, KeyboardHandler {
+  Paddle({
+    required this.cornerRadius,
+    required super.position,
+    required super.size,
+  }) : super(anchor: Anchor.center, children: [RectangleHitbox()]);
+
+  final Radius cornerRadius;
+
+  final _paint = Paint()
+    ..color = const Color(0xff1e6091)
+    ..style = PaintingStyle.fill;
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    final keysPressed = HardwareKeyboard.instance.logicalKeysPressed;
+    if (keysPressed.contains(LogicalKeyboardKey.arrowLeft) ||
+        keysPressed.contains(LogicalKeyboardKey.keyA)) {
+      position.x = (position.x - (dt * 500)).clamp(
+        width / 2,
+        game.width - width / 2,
+      );
+    } else if (keysPressed.contains(LogicalKeyboardKey.arrowRight) ||
+        keysPressed.contains(LogicalKeyboardKey.keyD)) {
+      position.x = (position.x + (dt * 500)).clamp(
+        width / 2,
+        game.width - width / 2,
+      );
+    }
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(Offset.zero & size.toSize(), cornerRadius),
+      _paint,
+    );
+  }
+
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+    if (isRemoved) return;
+    super.onDragUpdate(event);
+    position.x = (position.x + event.localDelta.x).clamp(
+      width / 2,
+      game.width - width / 2,
+    );
+  }
+}
+
+class Brick extends RectangleComponent
+    with CollisionCallbacks, HasGameReference<BrickBreaker> {
+  Brick(Vector2 position, Color color)
+      : super(
+    position: position,
+    size: Vector2(brickWidth, brickHeight),
+    anchor: Anchor.center,
+    paint: Paint()
+      ..color = color
+      ..style = PaintingStyle.fill,
+    children: [RectangleHitbox()],
+  );
+
+  @override
+  void onCollisionStart(Set<Vector2> intersectionPoints,
+      PositionComponent other,) {
+    super.onCollisionStart(intersectionPoints, other);
+    removeFromParent();
+
+    if (game.world.children
+        .query<Brick>()
+        .length == 1) {
+      game.startGame();
+    }
+  }
+}
+
+class PlayArea extends RectangleComponent with HasGameReference<BrickBreaker> {
+  PlayArea() : super(children: [RectangleHitbox()]);
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    size = Vector2(game.width, game.height);
+  }
+}
+
+const brickColors = [
+  Color(0xfff94144),
+  Color(0xffe1b8b9),
+  Color(0xff876f70),
+  Color(0xff756c6c),
+  Color(0xff8c8b8b),
+  Color(0xfff94144),
+  Color(0xffe1b8b9),
+  Color(0xff876f70),
+  Color(0xff756c6c),
+  Color(0xff8c8b8b),
+];
+
+const gameWidth = 820.0;
+const gameHeight = 1600.0;
+const ballRadius = gameWidth * 0.02;
+const paddleWidth = gameWidth * 1;
+const paddleHeight = ballRadius * 2;
+const paddleStep = gameWidth * 0.05;
+const brickGutter = gameWidth * 0.015;
+final brickWidth =
+    (gameWidth - (brickGutter * (brickColors.length * 1))) / brickColors.length;
+const brickHeight = gameHeight * 0.03;
+const difficultyModifier = 1.05;

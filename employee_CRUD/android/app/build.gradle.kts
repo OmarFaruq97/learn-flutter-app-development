83f87b86f003364d19e63201644c2b4f6c85b7f7
Omar Faruq <omarfaruq9723@gmail.com>
2025-06-22 15:01:18 +0600

    init

--- employee_CRUD/android/app/build.gradle.kts
+++ employee_CRUD/android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.org.isdb62.employee_crud"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+//    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
